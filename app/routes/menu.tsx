5f7053e42c8be9b8212f28c9b9cb3ec0cb058cef
Amar Dhillon <87444806+AmarDhillon05@users.noreply.github.com>
2025-03-30 05:19:14 -0400

    Update menu.tsx

--- app/routes/menu.tsx
+++ app/routes/menu.tsx
@@ -10,7 +10,8 @@ export default function Menu() {
   async function getData() {
     const response = await fetch(dburi);
     let data = await response.json();
-    data = data.menu;
+    //data = data.menu; ONLY FOR SIDHARTHS DATABASE
+    data = JSON.parse(data)
     Object.keys(data)
       .filter((key) => data[key].name !== "")
       .forEach((key) => {
@@ -63,6 +64,73 @@ export default function Menu() {
             Today's Menu
           </h1>
         </div>
+
+
+        <div className="flex flex-wrap justify-start px-8 py-4 bg-white shadow-md rounded-lg mb-8">
+                <p className="font-semibold text-lg">Filter by:</p>
+
+                <div className="flex items-center mx-4">
+                    <input type="radio" id="calories" name="filter" className="appearance-none w-5 h-5 border-2 border-gray-600 checked:bg-blue-600 checked:border-blue-600 mr-2 cursor-pointer" 
+                    onClick={() => {
+                        let copy = JSON.parse(JSON.stringify(menu)); 
+
+                        let values = Object.values(copy).sort((a, b) => {
+                            if (Object.keys(a).includes('cal') && Object.keys(b).includes('cal')) {
+                                return a.cal - b.cal; 
+                            } else {
+                                return -1;
+                            }
+                        });
+
+                        values = {...values}
+                        setMenu(values)
+                    }}
+                    />
+                    <label htmlFor="calories" className="text-md">Calories</label>
+                </div>
+
+                <div className="flex items-center mx-4">
+                    <input type="radio" id="protein" name="filter" className="appearance-none w-5 h-5 border-2 border-gray-600 checked:bg-blue-600 checked:border-blue-600 mr-2 cursor-pointer" 
+                    onClick={() => {
+                        let copy = JSON.parse(JSON.stringify(menu)); 
+
+                        let values = Object.values(copy).sort((a, b) => {
+                            if (Object.keys(a).includes('cal') && Object.keys(b).includes('cal')) {
+                                return b.protein - a.protein; 
+                            } else {
+                                return -1;
+                            }
+                        });
+
+                        values = {...values}
+                        setMenu(values)
+                    }}
+                    />
+                    <label htmlFor="protein" className="text-md">Protein</label>
+                </div>
+
+                <div className="flex items-center mx-4">
+                    <input type="radio" id="cal-protein" name="filter" className="appearance-none w-5 h-5 border-2 border-gray-600 checked:bg-blue-600 checked:border-blue-600 mr-2 cursor-pointer" 
+                    onClick={() => {
+                        let copy = JSON.parse(JSON.stringify(menu)); 
+
+                        let values = Object.values(copy).sort((a, b) => {
+                            if (Object.keys(a).includes('cal') && Object.keys(b).includes('cal')) {
+                                return (a.cal/a.protein) - (b.cal/b.protein); 
+                            } else {
+                                return -1;
+                            }
+                        });
+
+                        values = {...values}
+                        setMenu(values)
+                    }}
+                    />
+                    <label htmlFor="cal-protein" className="text-md">Cal-Protein Ratio</label>
+                </div>
+            </div>
+
+
 
         <div className="container mx-auto px-4">
           {Object.keys(menu).map((key, idx) => {
